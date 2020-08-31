ad475b13b5ddc52317164c462b97794ae2f8a671
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-08-31 22:05:52 +0530

    # Little Twerks [1]
    Update Before Sleep.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from PyQt5.QtWidgets import *
-from PyQtGui import *
-from PyQt5 import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication
+# from PyQt5.QtGui import *
+# from PyQt5 import *
+# from PyQt5.QtCore import *
 from create_password import create_password_btn, create_button_btn
 import sys
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     a_window = Window()
     sys.exit(app.exec_())
     
