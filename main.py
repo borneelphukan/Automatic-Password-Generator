44e1236f4ac80ba39178655e0433a6c19abad20c
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-08-31 22:22:54 +0530

    # Main [Done]

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from PyQt5.QtWidgets import QApplication
-# from PyQt5.QtGui import *
-# from PyQt5 import *
-# from PyQt5.QtCore import *
+# Importing Libraries
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from create_password import create_password_btn, create_button_btn
 import sys
 
+# Window Class
+class Window(QMainWindow): 
+    
+    # Default Constructor
+    def __init__(self): 
+
+        # Super Method
+        super().__init__()
+        self.setWindowTitle("Password Generator")    # Title 
+        self.setGeometry(0, 0, 1000, 1000)           # Size of the Window
+        self.show() 
+
+# Main Method
 def main():
     app = QApplication(sys.argv)
     a_window = Window()
     sys.exit(app.exec_())
-    
-if __main__ == "__main__":
+
+# Execution Point
+if __name__ == "__main__":
     main()
